9edbee2915451bbeed5707de0e458dc0a286ca2c
Erik Danielsen <erik.steen.danielsen@gmail.com>
2012-08-21 13:49:21 +0200

    ....

---
+++ opgaver/sharp/KlapHestenConsole/KlapHestenConsole/PinsGameBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KlapHestenConsole
+{
+    abstract class PinsGameBase
+    {
+        protected int horses = 15;
+        protected Boolean play = true;
+
+        protected String[,] players = new string[2, 2];
+        protected int currentPlayer = 0;
+    
+        abstract public void playGame();
+
+        abstract public void endGame();
+
+        public Boolean validate(String arg)
+        {
+            short converted;
+
+            if (!Int16.TryParse(arg, out converted) || (converted <= 0 || converted > 3) || converted > horses)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void selectStartingPlayer()
+        {
+            currentPlayer = new Random().Next(0, players.GetLength(0) - 1);
+        }
+
+        public String getCurrentPlayerName()
+        {
+            return players[currentPlayer, 0];
+        }
+
+        public Boolean isComputerPlayer()
+        {
+            return players[currentPlayer, 1].Equals("auto");
+        }
+
+        public void grabHorse(String horsesToGrab)
+        {
+            int i = int.Parse(horsesToGrab);
+            Console.WriteLine("");
+            horses = (horses - i);
+        }
+
+        public void switchPlayer()
+        {
+            currentPlayer++;
+            if (currentPlayer > players.GetLength(0) - 1)
+            {
+                currentPlayer = 0;
+            }
+        }
+
+        public int autoPickNumber()
+        {
+            return new Random().Next(1, (horses > 3 ? 3 : horses));
+        }
+    }
+}
---
+++ opgaver/sharp/KlapHestenConsole/KlapHestenConsole/PinsGameConsole.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KlapHestenConsole
+{
+    class PinsGameConsole : PinsGameBase
+    {
+        public void writeTextLn(String arg)
+        {
+            Console.WriteLine(arg);
+        }
+
+        public void writeText(String arg)
+        {
+            Console.Write(arg);
+        }
+
+        public String readText()
+        {
+            return Console.ReadLine();
+        }
+
+        public void initializeGame()
+        {
+            writeTextLn("Welcome to sticks game! :)");
+            writeTextLn("");
+            writeTextLn("One or Two player? (1 or 2)");
+            if (int.Parse(readText()) == 1)
+            {
+                initializeOnePlayerGame();
+            }
+            else
+            {
+                initializeTwoPlayerGame();
+            }
+
+            selectStartingPlayer();
+        }
+
+        public void initializeTwoPlayerGame()
+        {
+            Console.WriteLine("Enter Player One name:");
+            players[0, 0] = Console.ReadLine();
+            players[0, 1] = "manual";
+            Console.WriteLine("Enter Player Two name:");
+            players[1, 0] = Console.ReadLine();
+            players[1, 1] = "manual";
+        }
+
+        public void initializeOnePlayerGame()
+        {
+            Console.WriteLine("Enter Player One name:");
+            players[0, 0] = Console.ReadLine();
+            players[0, 1] = "manual";
+
+            players[1, 0] = "Computer";
+            players[1, 1] = "auto";
+        }
+
+        public void printStatus()
+        {
+            Console.WriteLine("There is " + horses + " horses left.");
+        }
+
+        public void printPick()
+        {
+            Console.Write(getCurrentPlayerName() + ": " + "Pick a number between 1 and " + (horses > 3?"3":"2") + ": ");
+        }
+
+        override public void playGame()
+        {
+            initializeGame();
+            while (play)
+            {
+                printStatus();
+                printPick();
+                String arg = "";
+                if (isComputerPlayer())
+                {
+                    arg = autoPickNumber().ToString();
+                    Console.WriteLine(arg);
+                }
+                else
+                {
+                    arg = Console.ReadLine();
+                }
+
+                if (validate(arg))
+                {
+                    grabHorse(arg);
+                    switchPlayer();
+
+                    if (horses < 2)
+                    {
+                        play = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Argument not valid!");
+                }
+
+
+            }
+
+            endGame();
+        }
+
+        override public void endGame()
+        {
+            switchPlayer();
+            Console.WriteLine("Sorry " + getCurrentPlayerName() + " you lost.");
+            switchPlayer();
+            Console.WriteLine(getCurrentPlayerName() + " won the game.");
+        }
+
+        static void Main(string[] args)
+        {
+            PinsGameConsole horse = new PinsGameConsole();
+            horse.playGame();
+            Console.Read();
+        }
+
+        
+    }
+}
